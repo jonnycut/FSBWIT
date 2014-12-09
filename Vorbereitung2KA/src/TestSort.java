d65ff369b986202178f94c530a4eeab97a3586d4
KNapret <jonnycut@yahoo.de>
2014-12-09 09:27:54 +0100

    Sort

--- Vorbereitung2KA/src/TestSort.java
+++ Vorbereitung2KA/src/TestSort.java
@@ -3,7 +3,7 @@ public class TestSort {
 
 	public static void main(String[] args) {
 		
-		int[] array = {5,8,9,0,7,1,4,2,3,6};
+		int[] array = {5,8,9,0,7,1,4,2,3,6,10,2300,2135,12};
 		arraySort aS = new arraySort();
 		
 		for (int i = 0; i < array.length; i++)
