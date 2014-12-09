8c53eed09544216b69cf8f4fcd6945afddf52ef6
KNapret <jonnycut@yahoo.de>
2014-12-09 09:27:21 +0100

    Insert Sort

--- Weihnachtsbaum/src/TesteWeihnachtsbaum.java
+++ Weihnachtsbaum/src/TesteWeihnachtsbaum.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class TesteWeihnachtsbaum {
 
@@ -5,6 +7,8 @@ public class TesteWeihnachtsbaum {
 		
 		
 		Weihnachtsbaum[] wald = new Weihnachtsbaum[20];
+		Scanner s = new Scanner(System.in);
+		
 		
 		
 		for (int i=0; i< 20; i++){
@@ -14,42 +18,105 @@ public class TesteWeihnachtsbaum {
 			
 		}
 		
-	
+		System.out.println("Sortierung aufsteigend: 1");
+		System.out.println("Sortierung absteigend: 2");
+		System.out.println("Sortierung Insert: 3");
+		int auswahl = s.nextInt();
 		
-		Weihnachtsbaum[] sortBaum = new Weihnachtsbaum[20];
-		
-		for (int g = 0; g < 20; g++){
-			int kleinStelle =0;
-			double kleinster = 1.80;
-			Weihnachtsbaum kleinsterB = null;
-		
-			for (int i = 0, j= 0; i < wald.length; i++ ){
-				
-				if (wald[i] != null){ 
-					if (wald[i].getGroesse() < kleinster){
+			switch (auswahl) {
+				case 1:
+					
+					Weihnachtsbaum[] sortBaum = new Weihnachtsbaum[20];
+					
+					for (int g = 0; g < 20; g++){
+						int kleinStelle =0;
+						double kleinster = 1.80;
+						Weihnachtsbaum kleinsterB = null;
+					
+						for (int i = 0, j= 0; i < wald.length; i++ ){
+							
+							if (wald[i] != null){ 
+								if (wald[i].getGroesse() < kleinster){
+									
+									kleinster = wald[i].getGroesse();
+									kleinsterB = wald[i];
+									kleinStelle = i;
+									
+								}
+								
+							}
+						}
 						
-						kleinster = wald[i].getGroesse();
-						kleinsterB = wald[i];
-						kleinStelle = i;
+						wald[kleinStelle]=null;
+						sortBaum[g] = kleinsterB;
 						
 					}
+					System.out.println("Der kleinste ist " + sortBaum[0].getGroesse());
+					for (Weihnachtsbaum k:sortBaum){
+						System.out.println(k.getGroesse());
+					}
+					break;
 					
-				}
-			}
-			
-			wald[kleinStelle]=null;
-			sortBaum[g] = kleinsterB;
-			
+				case 2:
+					
+					Weihnachtsbaum[] sortBaumAb = new Weihnachtsbaum[20];
+					
+					for (int g = 0; g < 20; g++){
+						int groessteStelle = 0;
+						double groesster = 1.10;
+						Weihnachtsbaum groessterB = null;
+						
+						for (int i= 0 ; i < wald.length; i ++){
+							
+							if (wald[i] != null){
+								if (wald[i].getGroesse() > groesster){
+									groesster = wald[i].getGroesse();
+									groessterB = wald[i];
+									groessteStelle = i;
+								}
+							}
+						}
+						wald[groessteStelle] = null;
+						sortBaumAb[g] = groessterB;
+						
+					}
+					System.out.println("Der groesste ist: " +sortBaumAb[0].getGroesse());
+					for (Weihnachtsbaum k:sortBaumAb){
+						
+						System.out.println(k.getGroesse());
+					}
+					break;
+				
+				case 3:
+					
+					Weihnachtsbaum[] sortBaumInsert = new Weihnachtsbaum[20];
+					
+					for(int g = 0; g < wald.length; g++){
+						
+							Weihnachtsbaum puffer = null;
+							sortBaumInsert[g] = wald[g];
+							
+							for (int i = g; i > 0; i--){
+								if (sortBaumInsert[i].getGroesse() < sortBaumInsert[i -1].getGroesse()){
+									puffer = sortBaumInsert[i];
+									sortBaumInsert[i] = sortBaumInsert[i-1];
+									sortBaumInsert[i-1] = puffer;
+									
+								}
+							}
+						
+					}
+					for (Weihnachtsbaum k: sortBaumInsert){
+						System.out.println(k.getGroesse());
+					}
+					
 		}
-		System.out.println("Der kleinste ist " + sortBaum[0].getGroesse());
-		for (Weihnachtsbaum k:sortBaum){
-			System.out.println(k.getGroesse());
-		}
-		
-		
-		
+			
 		
 
 	}
 
+
 }
+
+	
