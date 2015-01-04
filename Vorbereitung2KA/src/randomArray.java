ff1053f8ea8bfafa9e6cd91e37508fe622260313
JonnyCut <JonnyCut@Maschinska>
2015-01-04 18:13:32 +0100

    multidimensional array sort

---
+++ Vorbereitung2KA/src/randomArray.java
@@ -0,0 +1,58 @@
+
+public class randomArray {
+	
+	
+
+public String[][] fuellen(String eingabe, String[][] array){
+	
+	String[][] newArray = new String[array.length +1][3];
+	
+	
+	for(int i=0; i<array.length; i++){
+		
+		newArray[i] = array[i];
+	}
+	newArray[newArray.length -1] = eingabe.split(";");
+	
+	
+	return newArray;
+	
+	} //ende fuellen
+
+	public void ausgabe(String[][] array){
+		System.out.println("DG    |Name      | Vorname");
+		System.out.println("-------------------------");
+		for(int i=0; i< array.length;i++){
+			for (int j=0; j<array[i].length; j++){
+				System.out.print(array[i][j]+"     | ");
+			}
+			System.out.println();
+		}
+	} //end ausgabe
+	
+	
+	public String[][] sortieren(String[][] array){
+		
+		String[] pufferArray = new String[array[0].length];
+		boolean tausch = true;
+		
+		for (int j=0;tausch;j++){
+			tausch = false;			
+			
+			for(int i=0; i< array.length -1;i++){
+				if (array[i][1].compareTo(array[i+1][1])>0){
+					pufferArray =array[i];
+					array[i] = array[i+1];
+					array[i+1] = pufferArray;
+					tausch = true;
+					
+				}
+			}
+		}
+		
+		return array;
+		
+		
+	}
+
+} //end class
---
+++ Vorbereitung2KA/src/randomMain.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+
+public class randomMain {
+
+	
+	public static void main(String[] args) {
+		
+		String[][] array = {{"OFR","MIST","GROßER"},
+						   	{"OFR","ZUNDER","BRENNTWIE"},
+						   	{"OFR","PELZ","PASCAL"},
+						   	{"OFR","TIME","JUSTIN"},
+						   	{"OFR","ENHEIM","FRANK"},
+						   	{"OFR","ANFANG","ALEX"}};
+		
+		randomArray rA = new randomArray();
+		Scanner s = new Scanner(System.in);
+		
+		for(;true;){
+			int auswahl = 0;
+			
+			
+			
+			System.out.println("Was möchten Sie tun?");
+			System.out.println("1. - Datensatz hinzufügen");
+			System.out.println("2. - Array sortieren");
+			System.out.println("3. - Array anzeigen");
+			System.out.println("4. - Zufallsausgabe eines Datensatzes");
+			
+			auswahl = s.nextInt();
+			
+			switch(auswahl){
+			
+				case 1:
+					
+					System.out.println("Bitte nächsten Datensatz eingeben: (DG;Nachname;Vorname");
+					array = rA.fuellen(s.next(), array);
+					break;
+					
+				case 2:
+					array = rA.sortieren(array);
+					break;
+					
+				case 3:
+					rA.ausgabe(array);
+					break;
+				
+				case 4:
+					//random
+					break;
+				
+				default:
+					System.out.println("Ungültige Eingabe");
+					break;
+					
+			}
+		} //end for:ture
+		
+		
+		
+
+	} //end main
+
+}
