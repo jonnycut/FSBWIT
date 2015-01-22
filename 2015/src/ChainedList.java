018768bfe2e231b2f42449657dc071aa61f7dd63
KNapret <jonnycut@yahoo.de>
2015-01-22 09:35:02 +0100

    rekursive chainedList methods added

--- 2015/src/ChainedList.java
+++ 2015/src/ChainedList.java
@@ -32,11 +32,10 @@ public class ChainedList {
 				pointer = pointer.getNext();
 				counter ++;
 				
-			}
-
-			
+			}			
 		return counter;
 	}
+	
 	
 	public void delete(String value){
 		
@@ -73,5 +72,100 @@ public class ChainedList {
 	}
 	
 	
+	
+//-----------------------------------------------Rekursion----------------------------------------------------------------------//
+	public int getSizeRekursiv(){
+		if (isEmpty()) return -1;
+		return getSizeRekursiv(first);
+	}
+	
+	private int getSizeRekursiv(ChainedElement element){
+				
+		if(element.getNext() != null){
+		return getSizeRekursiv(element.getNext()) +1;
+		}else return 1;
+	}
+	
+	public int getIndex(String text){
+		if (isEmpty()) return -1;
+		return getIndex(text, first, getSize() -1);
+		
+		
+	}
+	private int getIndex(String text, ChainedElement element, int counter){
+	
+		if(text.equals(element.getValue())) {
+			return counter;
+		}else if (element.getNext() == null){
+			return -1;
+		}else return getIndex(text,element.getNext(),counter-1);
+				
+				
+	}
+	
+	public String atIndex(int index){
+		if(isEmpty()) return "Liste leer";
+		return atIndex(index,first,getSize()-1);
+	}
+	
+	private String atIndex(int index, ChainedElement element,int counter){
+		
+		if(counter == index){ 
+			return element.getValue();
+		}else if (element.getNext() ==null){
+			return "index " + index + " nicht vorhanden";
+		}else return atIndex(index,element.getNext(),counter -1);
+		
+	}
+	
+	public String removeAt(int index){
+		if(isEmpty()) return "Index "+index + " nicht vorhanden";
+		return removeAt(index,first,getSize()-1);
+	}
+	
+	private String removeAt(int index, ChainedElement element, int counter){
+		ChainedElement puffer;
+		if(element.getNext()==null) 
+			return "Index "+index+" nicht vorhanden!";
+		if(index == getSize()-1){
+			puffer = first;
+			first = first.getNext();
+			return puffer.getValue();
+		}
+		if(counter -1 ==0){ 
+			puffer = element.getNext();
+			element.setNext(null);
+			return puffer.getValue();
+		}
+		if(counter-1 ==index){
+			puffer = element.getNext();
+			element.setNext(element.getNext().getNext()); 
+			return puffer.getValue();
+			
+		}else return removeAt(index, element.getNext(), counter -1);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//-----------------------------------------------EndRekursion-------------------------------------------------------------------//	
 
 }
--- 2015/src/ChainedListMain.java
+++ 2015/src/ChainedListMain.java
@@ -3,14 +3,21 @@ public class ChainedListMain {
 
 	public static void main(String[] args) {
 		ChainedList CL = new ChainedList();
-		String[] stringArray = {"erster","zweiter","dritter","vierter","fünfter","sechster","siebter"};
+		String[] stringArray = {"erster","zweiter","dritter","vierter","fünfter","sechster","siebter","achter"};
 		
 		for(int i=0; i<stringArray.length;i++)
 			CL.add(stringArray[i]);
 		
 		
 		
-		System.out.println(CL.getSize());
+		System.out.println(CL.getSizeRekursiv());
+		System.out.println(CL.getIndex("erster"));
+		System.out.println("Deleted String: " +CL.removeAt(7));
+		System.out.println();
+		System.out.println("Neue Liste:");
+		for (int i=0; i<CL.getSize(); i++){
+			System.out.println(CL.atIndex(i));
+		}
 		
 			
 		
