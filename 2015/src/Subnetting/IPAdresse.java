114f86398b089d990cc8e941a1f6496b5f7decd8
KNapret <jonnycut@yahoo.de>
2015-02-18 10:09:26 +0100

    getOkt abgeändert --> int operation

--- 2015/src/Subnetting/IPAdresse.java
+++ 2015/src/Subnetting/IPAdresse.java
@@ -2,7 +2,7 @@ package Subnetting;
 
 public class IPAdresse {
 	private int ipAdd;
-	private String ipBin;	//contains binari String of ipAdd
+	private String ipBin;	//contains binary String of ipAdd
 	
 	
 	
@@ -10,7 +10,7 @@ public class IPAdresse {
 		this.ipAdd=ipAdd;
 		ipBin=Integer.toBinaryString(ipAdd);
 		
-		while(ipBin.length()<32){ 				//ipBin auf 32 Stellen auffüllen
+		while(ipBin.length()<32){ 				//ipBin auf 32 Stellen auffï¿½llen
 			ipBin="0"+ipBin;
 		}
 	}
@@ -65,23 +65,8 @@ public class IPAdresse {
 		return ipBin;
 	}
 	
-	public int getOkt(int okt){				//liefert das entsprechende Oktett der IP zur basis 10 zurück (ausgehend vom Binärstring)
-		
-		switch(okt){
-			
-			case 1:
-				return Integer.parseInt(ipBin.substring(0, 8),2);
-			case 2:
-				return Integer.parseInt(ipBin.substring(8, 16),2);
-			case 3:
-				return Integer.parseInt(ipBin.substring(16, 24),2);
-			case 4:
-				return Integer.parseInt(ipBin.substring(24),2);
-			default:
-				return -1;
-				
-		}
-		
+	public int getOkt(int okt){				//liefert das entsprechende Oktett der IP zur basis 10 zurÃ¼ck (ausgehend vom Binï¿½rstring)
+		return ((ipAdd>>(4-okt)*8)&255);
 	}
 	
 	public String toString(){
@@ -93,26 +78,3 @@ public class IPAdresse {
 }//end class
 
 
-
-//public IPAdresse(long gZahl){
-//	this.gZahl=gZahl;
-//	
-//	String[] bin=new String[4];
-//	String puffer = Long.toBinaryString(gZahl);
-//	int start=0;
-//	int end=8;
-//	int tmp=0;
-//	String dez="";
-//	
-//	for(int i=0; i<4;i++){
-//		bin[i]=puffer.substring(start,end);
-//		tmp = Integer.parseInt(bin[i],2);
-//		dez=dez+tmp+".";
-//		start=end;
-//		end = end +8;
-//		
-//	}
-//	this.dez=dez;
-//	this.bin=bin;
-//	
-//}
