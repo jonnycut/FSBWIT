d7b4a9358cf28dd216cbd57a0425476b0d199f06
KNapret <jonnycut@yahoo.de>
2015-03-23 09:30:20 +0100

    Puffer erweitert

--- 2015/src/Puffer/Puffer.java
+++ 2015/src/Puffer/Puffer.java
@@ -12,10 +12,32 @@ public class Puffer<T> {
 		
 		for(int i=0; i<=size; i++){
 			tmp.next = new Element();
+			tmp = tmp.next;
 			
 		}
 		tmp.next=lesen;
 		schreiben = lesen;
+	}
+	
+	public boolean isEmpty(){
+		return (lesen==schreiben);
+	}
+	
+	public boolean isFull(){
+		return (schreiben.next == lesen);
+		
+	}
+	
+	public T lesen(){
+		T tmp = lesen.value;
+		lesen = lesen.next;
+		return tmp;
+	}
+	
+	public void schreiben(T value){
+		schreiben.value = value;
+		schreiben = schreiben.next;
+		
 	}
 	
 	private class Element{
--- 2015/src/Puffer/Test.java
+++ 2015/src/Puffer/Test.java
@@ -3,8 +3,22 @@ package Puffer;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Puffer<Integer> p =new Puffer<>(50);
+		
+		for(int i=0; i<1000;){
+			
+			int weite = (int)(Math.random()*230);
+			for(int x=0; x<=weite ;x++){
+				if(p.isFull())break;
+				p.schreiben(i);
+				i++;
+				if(i==1001) break;
+			}
+			for(int y=0; y<weite;y++){
+				if(p.isEmpty()) break;
+				System.out.println(p.lesen());
+			}
+		}
 	}
 
 }
