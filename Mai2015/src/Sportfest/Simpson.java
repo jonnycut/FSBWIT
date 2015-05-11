62f4905e252d914d54ef647effa0b51bdd97e7d8
KNapret <jonnycut@yahoo.de>
2015-05-11 14:54:22 +0200

    Simpsons Sportfest angefangen!

---
+++ Mai2015/src/Sportfest/Simpson.java
@@ -0,0 +1,47 @@
+package Sportfest;
+
+public class Simpson implements Comparable<Simpson> {
+	
+	private String name;
+	
+	public Simpson(String name){
+		
+		this.name = name;
+		
+	}
+
+	@Override
+	public int compareTo(Simpson s1) {
+		
+		return this.compareTo(s1);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Simpson other = (Simpson) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
